d034647e990b6c375a41004b7931bcccaeef4c48
umar.usman <umar.usman@tenpearls.com>
2018-11-13 10:10:13 +0500

    CORS allowed

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import * as Koa from 'koa';
+import * as CORS from '@koa/cors';
 import * as bodyparser from 'koa-bodyparser';
 
 import {routes} from './routes/index';
 import { responseHandler } from './middlewares/responseHandler';
 
 const app = new Koa();
+
+app.use(CORS());
 
 app.use(responseHandler);
 
--- src/models/db.ts
+++ src/models/db.ts
@@ -1,12 +1,6 @@
 import * as mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/MyContacts', { useNewUrlParser: true});
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
-  console.log("db connection successful");
-});
+mongoose.connect('mongodb://localhost:27017/MyContacts', { useNewUrlParser: true });
+mongoose.connection.on('error', console.error);
 
 export default mongoose;
