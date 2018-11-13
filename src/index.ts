e2889cc5a4ad1dfd042bc463f131d4b4e00700bb
hamza1100 <hamza1100@github.com>
2018-11-13 20:06:09 +0500

    SocialLogin WIP

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,36 @@ app.use(responseHandler);
 
 app.use(bodyparser());
 
+
+
 app.use(routes);
+
+import * as passport from 'koa-passport';
+// const GoogleStrategy = require('passport-google-auth').Strategy
+
+import * as pag from 'passport-google-oauth2';
+const GoogleStrategy = pag.Strategy
+
+//const GoogleStrategy = OAuthStrategy;
+
+passport.use(new GoogleStrategy({
+    clientID: '502002563979-rtq9ausgvoou62rspe4rhujlocaolcnq.apps.googleusercontent.com',
+    clientSecret: 'xSrCWrhIWpoMDbEgnGTSLpdv',
+    callbackURL: 'http://localhost:' + (process.env.PORT || 4000) + '/auth/google/callback',
+    passReqToCallback   : true
+  },
+  function(request: any, accessToken: any, refreshToken: any, profile: any, done: any) {
+      const t = request;
+      const s = accessToken;
+      const refreshTokena = refreshToken;
+      const sa = accessToken;
+      const d = done;
+    //   console.log(token, tokenSecret, profile, done);
+    // retrieve user ...
+    console.log(profile)
+    return done();
+  }
+))
 
 app.listen(4000, () => {
     console.log('Server started.');
--- src/routes/test.ts
+++ src/routes/test.ts
@@ -1,9 +1,32 @@
+import * as passport from 'koa-passport';
 import * as Router from 'koa-router';
 
 import * as ctrl from '../controllers/test';
+import { Context } from 'koa';
 
 const router = new Router();
 
 router.get('/test', ctrl.testApi);
+
+router.get('/auth/google',
+    passport.authenticate('google', { scope:
+        [ 'https://www.googleapis.com/auth/plus.login',
+          'https://www.googleapis.com/auth/plus.profile.emails.read' ] })
+)
+
+router.get('/app', (ctx: Context) => {
+    ctx.body = 'hello hi';
+});
+
+router.get('/', (ctx: Context) => {
+    ctx.body = 'lol';
+})
+
+router.get('/auth/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/app',
+    failureRedirect: '/'
+  })
+);
 
 export default router.routes();
